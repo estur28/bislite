a1cf0e7586a9c185932f60e6017f1e6aec89c998
Natalia <safary-1994@mail.ru>
2020-10-26 17:08:55 +0300

    feat: layot all

--- js/main.js
+++ js/main.js
@@ -5,5 +5,13 @@ $(function() {
         dots: true,
 
     });
+
+
+    $('.works__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/ar-left.svg" alt="ar-left"></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/ar-right.svg" alt="ar-right"></button>',
+    });
      
 });
