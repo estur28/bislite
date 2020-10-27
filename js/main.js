bb380bb4997e1286d868849ec6968650ea794669
Natalia <safary-1994@mail.ru>
2020-10-27 13:36:20 +0300

    feat: add accesibile settings

--- js/main.js
+++ js/main.js
@@ -12,6 +12,36 @@ $(function() {
         slidesToScroll: 1,
         prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/ar-left.svg" alt="ar-left"></button>',
         nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/ar-right.svg" alt="ar-right"></button>',
-    });
+        responsive:[
+            {
+                breakpoint: 911,
+                settings: {
+                  slidesToShow: 3,
+                }
+              },
+              {
+                breakpoint: 681,
+                settings: {
+                  slidesToShow: 2,
+                }
+              },
+              {
+                breakpoint: 455,
+                settings: {
+                  slidesToShow: 1,
+                }
+              },
+            ]
+          });
+
+
+    $('.header__menu-btn').on('click', function(){
+        $('.header__menu > ul').slideToggle();
+    }); 
+
+     $('.header__drop-down.drop-down').on('click', function(){
+        $(this).children('.drop-down__list').toggleClass('active');
+        $(this).children('.drop-down__link').toggleClass('active');
+     }); 
      
 });
